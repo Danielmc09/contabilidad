bf9607c2217aaf2a0b23f2edcda059a4b073f79d
Daniel Mendieta <danielmc0911@gmail.com>
2025-06-30 20:21:58 -0500

    init

---
+++ asientos_contables/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AsientosContablesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "asientos_contables"
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from contabilidad!")
+
+
+if __name__ == "__main__":
+    main()
